43caeefb499bec47031d359d2d0fdae54c3bde2c
Nagashree01 <nagahegde2001@gmail.com>
2022-04-09 18:22:17 +0530

    added footer component

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -4,7 +4,19 @@ import "./footer.css";
 export default function Footer(){
     return (
         <div>
-            
+            <footer className="footer">
+                <div className="footer-header text-center text-size-normal">Made with code by Nagashree Hegde</div>
+                <ul className="social-links flex ">
+                    <li className="social-link-list">
+                        <a className="link" href="https://github.com/Nagashree01" rel="noreferrer">
+                        <i className="fab fa-github text-size-m"></i></a>
+                    </li>
+                    <li className="social-link-list">
+                        <a className="link" href="https://www.linkedin.com/in/nagashree-hegde-96876417b/" rel="noreferrer">
+                        <i className="fab fa-linkedin-in text-size-m"></i></a>
+                    </li>
+                </ul>
+            </footer>
         </div>
     )
 }
